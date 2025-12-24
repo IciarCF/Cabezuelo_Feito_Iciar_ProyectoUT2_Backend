938149cd595034fa541d4453379522f1e8623578
Iciar Cabezuelo <iciar.cabezuelo@gmail.com>
2025-12-24 11:23:36 +0100

    Backend Final 2.0

--- config/mongoConfig.js
+++ config/mongoConfig.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-/*
+/**
 Usamos "export" para poder exportar la funcion a otras zonas de nuestro proyecto.
 Esta funcion sirve para cargar la configuracion de MongoDB
 */
 export function loadMongoConfig(){
+    //Esta Url nos permite conectarnos a la BD
     const mongoURI = "mongodb+srv://iciarcabezuelo:Admin1234@clusterproyectoligadepo.3fahpbg.mongodb.net/LigaDeportiva?appName=ClusterProyectoLigaDeportiva"
+    //Conectamos a la BBDD
     mongoose.connect(mongoURI)
-  .then(() => console.log("¡¡¡Conectado a MongoDB!!!"))
-  .catch(err => console.log("Error al conectarse a MongoDB :(", err));
+    // Aparecerá un mensaje en consola para saber si se ha conectado bien o no.
+    .then(() => console.log("¡¡¡Conectado a MongoDB!!!"))
+    .catch(err => console.log("Error al conectarse a MongoDB :(", err));
 }
--- index.js
+++ index.js
@@ -1,40 +1,52 @@
-import express from 'express';
-import { loadMongoConfig } from './config/mongoConfig.js';
-import User from './model/User.js';
-import cors from 'cors';
+import express from 'express'; //Framework para crear el servidor y rutas HTTP
+import { loadMongoConfig } from './config/mongoConfig.js'; //Importa una funcion para conectar el backend al MongoDB
+import User from './model/User.js'; //Importa el modelo User (los datos).
+import cors from 'cors'; //Importa CORS, que permite que Angular haga las peticioens al backend
+
+/*
+    app -> crea nuestro servidor.
+    PORT -> indica el puerto por que va a recibir peticiones.
+*/
 const app = express();
 const PORT = 3000;
 
+//Funcion para arrancar el servidor.
 app.listen(PORT, () =>{
     console.log("Escuchando por el puerto " + PORT)
 })
 
-loadMongoConfig();
 
-app.use(cors())
+loadMongoConfig();//Conexion a MongoDB
+
+app.use(cors()); //Permite a Angular acceder al backend (Middleware)
 app.use(express.json()); //Indicamos qu elas peticiones va a ser siempre en formato .json
 
+// Esta funcion nos permite crear usuarios
 app.post("/api/users",async (req,res) =>{
+    //Controlamos si ocurren errores con un try-catch
     try{
-        const userData = req.body;
-        const newUser = new User(userData);
-        await newUser.save();
-        res.status(201).json({message:"Usuario creado"});
+        const userData = req.body; //Obtiene datos enviados por Angular
+        const newUser = new User(userData); //Crea un objeto usando el modelo de User
+        await newUser.save(); //Guarda los datos en MongoDB
+        res.status(201).json({message:"Usuario creado"}); //Devuelebe un mensaje JSON.
     }catch(err){
         console.log(err);
         res.status(500).json({menssage:"Error al crear el usuario"})
     }
 })
 
+//Esta funcion permite que los usuarios se registren
 app.post("/api/login", async(req, res) =>{
+    
     try{
+        //Obtiene usuario y contraseña de la peticion de Angular.
         const {username, password} = req.body;
-
+        
+        // Variable con datos de usuario y contraseña
         const user = await User.findOne({username,password});
         if(!user){
             return res.status(401).json({message:"Error autenficación"});
         }
-
         res.status(200).json({message:"Sesion iniciada", user})
     }catch(err){
         console.log(err);
